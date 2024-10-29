478070a86f6a82ea80c04cdbfa80555a945239e9
stgHellis <etienneake@hotmail.fr>
2024-10-29 20:43:16 +0100

    Simple connexion à Github

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,12 +5,15 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import LoaderSpinner from "@/components/LoaderSpinner";
 
 const Home = () => {
   const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
 
+  if (!trendingPodcasts) return <LoaderSpinner />;
+
   return (
-    <div className="mt-9 flex flex-col gap-9">
+    <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcastr</h1>
 
